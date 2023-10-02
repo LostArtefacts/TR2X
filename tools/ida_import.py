488d0aed41ca3cd38ae7e4fd59141ef224e78cbd
Marcin Kurczewski <dash@wind.garden>
2023-10-02 11:04:35 +0200

    tools: improve variable support in ida importer

--- tools/ida_import.py
+++ tools/ida_import.py
@@ -4,7 +4,7 @@ import re
 import tempfile
 from collections import defaultdict
 from dataclasses import dataclass
-from enum import Enum
+from enum import StrEnum
 from pathlib import Path
 from typing import Any, Dict, List, Optional
 
@@ -20,23 +20,31 @@ DOCS_DIR = REPO_DIR / "docs"
 PROGRESS_FILE = DOCS_DIR / "progress.txt"
 
 
-class ProgressFileSection(Enum):
+class ProgressFileSection(StrEnum):
     TYPES = "types"
     FUNCTIONS = "functions"
     VARIABLES = "variables"
 
 
 @dataclass
-class Declaration:
+class Symbol:
     offset: int
-    declaration: str
+    signature: str
     flags: str = ""
 
 
 @dataclass
 class ProgressFile:
     type_definitions: str
-    declarations: list[Declaration]
+    functions: list[Symbol]
+    variables: list[Symbol]
+
+
+@dataclass
+class ProgressFile:
+    type_definitions: str
+    functions: list[Symbol]
+    variables: list[Symbol]
 
 
 def to_int(source: str) -> Optional[int]:
@@ -54,36 +62,43 @@ def to_int(source: str) -> Optional[int]:
 
 
 def parse_progress_file(path: Path) -> ProgressFile:
-    current_section: ProgressFileSection | None = None
-    section_map: dict[ProgressFileSection.TYPES, list[str]] = defaultdict(list)
-    for line in path.read_text().splitlines():
-        for section in ProgressFileSection:
-            if line == f"# {section.value.upper()}":
-                current_section = section
+    result = ProgressFile(type_definitions="", functions=[], variables=[])
+
+    section: ProgressFileSection | None = None
+    for line in path.read_text(encoding="utf-8").splitlines():
+        line = line.strip()
+        if match := re.match("^# ([A-Z]+)$", line):
+            section_name = match.group(1).lower()
+            if section_name in list(ProgressFileSection):
+                section = ProgressFileSection(section_name)
+
         if line.startswith("#") or not line:
             continue
-        if current_section is not None:
-            section_map[current_section].append(line)
 
-    declarations: list[Declaration] = []
-    for line in section_map[ProgressFileSection.VARIABLES]:
-        offset, flags, declaration = re.split("\s+", line, maxsplit=2)
-        offset = to_int(offset)
-        declarations.append(
-            Declaration(offset=offset, declaration=declaration, flags=flags)
-        )
+        if section == ProgressFileSection.TYPES:
+            result.type_definitions += line + "\n"
 
-    for line in section_map[ProgressFileSection.FUNCTIONS]:
-        offset, size, flags, declaration = re.split("\s+", line, maxsplit=3)
-        offset = to_int(offset)
-        declarations.append(
-            Declaration(offset=offset, declaration=declaration, flags=flags)
-        )
+        if section == ProgressFileSection.FUNCTIONS:
+            offset, size, flags, signature = re.split(r"\s+", line, maxsplit=3)
+            result.functions.append(
+                Symbol(
+                    signature=signature,
+                    offset=to_int(offset),
+                    flags=flags,
+                )
+            )
 
-    return ProgressFile(
-        type_definitions="\n".join(section_map[ProgressFileSection.TYPES]),
-        declarations=declarations,
-    )
+        if section == ProgressFileSection.VARIABLES:
+            offset, flags, signature = re.split(r"\s+", line, maxsplit=2)
+            result.variables.append(
+                Symbol(
+                    signature=signature,
+                    offset=to_int(offset),
+                    flags=flags,
+                )
+            )
+
+    return result
 
 
 def import_types_from_file(path: Path) -> None:
@@ -95,39 +110,45 @@ def import_types_from_file(path: Path) -> None:
     print(f"    done ({error_count} errors)")
 
 
-def import_declaration(declaration: Declaration) -> None:
-    known = not re.match(r"(\s+|^)(dword|sub)_", declaration.declaration)
+def import_symbol(symbol: Symbol) -> None:
+    known = not re.match(r"(\s+|^)(dword|sub)_", symbol.signature)
 
     if known:
-        print(
-            f"    renaming 0x{declaration.offset:08x} to {declaration.declaration}"
-        )
+        if idc:
+            result = idc.parse_decl(symbol.signature, idc.PT_SILENT)
+            if not result:
+                raise RuntimeError("invalid signature", symbol.signature)
+
+            name = result[0]
+            if name.startswith("_"):
+                name = name[1:]
+
+            idc.set_name(symbol.offset, name)
+            idc.apply_type(symbol.offset, result)
 
         if idaapi:
             til = idaapi.get_idati()
             ti = idaapi.tinfo_t()
 
-            name = idaapi.parse_decl(
-                ti, til, declaration.declaration, idaapi.PT_VAR
-            )
+            name = idaapi.parse_decl(ti, til, symbol.signature, idaapi.PT_VAR)
             if not name:
-                raise RuntimeError('invalid declaration', name)
+                raise RuntimeError("invalid signature", symbol.signature)
 
             if name.startswith("_"):
                 name = name[1:]
 
-            idaapi.set_name(declaration.offset, name)
-            idaapi.apply_tinfo(declaration.offset, ti, 0)
+            idaapi.set_name(symbol.offset, name)
+            idaapi.apply_tinfo(symbol.offset, ti, 0)
 
     if idaapi:
-        func_num = idaapi.get_func_num(declaration.offset)
+        func_num = idaapi.get_func_num(symbol.offset)
         if func_num != -1:
             func_struct = idaapi.getn_func(func_num)
             if func_struct:
                 # BGR
-                if "+" in declaration.flags:
+                if "+" in symbol.flags:
                     func_struct.color = 0xA0FFA0
-                elif "x" in declaration.flags:
+                elif "x" in symbol.flags:
                     func_struct.color = 0xA0A0A0
                 elif known:
                     func_struct.color = 0xA0FFFF
@@ -136,14 +157,14 @@ def import_declaration(declaration: Declaration) -> None:
                 idaapi.update_func(func_struct)
 
 
-def import_functions_from_file(declarations: list[Declaration]) -> None:
-    print(f"Importing declarations:")
+def import_symbols(symbols: list[Symbol]) -> None:
+    print(f"Importing symbols ({len(symbols)}):")
     error_count = 0
-    for declaration in declarations:
+    for symbol in symbols:
         try:
-            import_declaration(declaration)
+            import_symbol(symbol)
         except Exception as ex:
-            print('  error', ex)
+            print("  error", ex)
             error_count += 1
     print(f"    done ({error_count} errors)")
 
@@ -158,7 +179,8 @@ def main():
         types_file.write_text(progress_file.type_definitions)
 
         import_types_from_file(types_file)
-        import_functions_from_file(progress_file.declarations)
+        import_symbols(progress_file.functions)
+        import_symbols(progress_file.variables)
 
 
 if __name__ == "__main__":
