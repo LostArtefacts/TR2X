c5b73e2c73b8d6bb6aeb0a8da285ee473f92f367
Marcin Kurczewski <dash@wind.garden>
2023-11-13 22:49:09 +0100

    effects: refactor Effect_Kill to be similar to items

--- src/game/effects.c
+++ src/game/effects.c
@@ -3,6 +3,45 @@
 #include "global/const.h"
 #include "global/vars.h"
 
+static void Effect_RemoveActive(const int16_t fx_num);
+static void Effect_RemoveDrawn(const int16_t fx_num);
+
+static void Effect_RemoveActive(const int16_t fx_num)
+{
+    struct FX_INFO *const fx = &g_Effects[fx_num];
+    int16_t link_num = g_NextEffectActive;
+    if (link_num == fx_num) {
+        g_NextEffectActive = fx->next_active;
+        return;
+    }
+
+    while (link_num != NO_ITEM) {
+        if (g_Effects[link_num].next_active == fx_num) {
+            g_Effects[link_num].next_active = fx->next_active;
+            return;
+        }
+        link_num = g_Effects[link_num].next_active;
+    }
+}
+
+static void Effect_RemoveDrawn(const int16_t fx_num)
+{
+    struct FX_INFO *const fx = &g_Effects[fx_num];
+    int16_t link_num = g_Rooms[fx->room_num].fx_num;
+    if (link_num == fx_num) {
+        g_Rooms[fx->room_num].fx_num = fx->next_fx;
+        return;
+    }
+
+    while (link_num != NO_ITEM) {
+        if (g_Effects[link_num].next_fx == fx_num) {
+            g_Effects[link_num].next_fx = fx->next_fx;
+            return;
+        }
+        link_num = g_Effects[link_num].next_fx;
+    }
+}
+
 void __cdecl Effect_InitialiseArray(void)
 {
     g_NextEffectFree = 0;
@@ -40,32 +79,8 @@ int16_t __cdecl Effect_Create(const int16_t room_num)
 void __cdecl Effect_Kill(const int16_t fx_num)
 {
     struct FX_INFO *const fx = &g_Effects[fx_num];
-
-    int16_t link_num = g_NextEffectActive;
-    if (link_num == fx_num) {
-        g_NextEffectActive = fx->next_active;
-    } else {
-        while (link_num != NO_ITEM) {
-            if (g_Effects[link_num].next_active == fx_num) {
-                g_Effects[link_num].next_active = fx->next_active;
-                break;
-            }
-            link_num = g_Effects[link_num].next_active;
-        }
-    }
-
-    link_num = g_Rooms[fx->room_num].fx_num;
-    if (link_num == fx_num) {
-        g_Rooms[fx->room_num].fx_num = fx->next_fx;
-    } else {
-        while (link_num != NO_ITEM) {
-            if (g_Effects[link_num].next_fx == fx_num) {
-                g_Effects[link_num].next_fx = fx->next_fx;
-                break;
-            }
-            link_num = g_Effects[link_num].next_fx;
-        }
-    }
+    Effect_RemoveActive(fx_num);
+    Effect_RemoveDrawn(fx_num);
 
     fx->next_fx = g_NextEffectFree;
     g_NextEffectFree = fx_num;
