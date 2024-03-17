LostArtefacts/TR2X

commit bd005bf0d39ade09424fe6ecb5c5cd0befada4ab

port Text_Create

src/game/text.c
@@ -1,9 +1,14 @@
 #include "game/text.h"
 
+#include "global/const.h"
 #include "global/funcs.h"
 #include "global/vars.h"
+#include "util.h"
+
+#include <string.h>
 
 #define TEXT_MAX_STRINGS 64
+#define TEXT_MAX_STRING_SIZE 64
 
 void __cdecl Text_Init(void)
 {
@@ -12,4 +17,52 @@ void __cdecl Text_Init(void)
         g_TextstringTable[i].flags.all = 0;
     }
     g_TextstringCount = 0;
+}
+
+struct TEXTSTRING *__cdecl Text_Create(
+    const int32_t x, const int32_t y, const int32_t z, const char *const text)
+{
+    if (text == NULL) {
+        return NULL;
+    }
+
+    struct TEXTSTRING *result = NULL;
+    for (int i = 0; i < TEXT_MAX_STRINGS; i++) {
+        struct TEXTSTRING *const string = &g_TextstringTable[i];
+        if (!string->flags.active) {
+            result = string;
+            break;
+        }
+    }
+
+    if (!result) {
+        return NULL;
+    }
+
+    const int32_t i = result - g_TextstringTable;
+    result->text = g_TextstringBuffers[i];
+    result->scale.h = PHD_ONE;
+    result->scale.v = PHD_ONE;
+    result->pos.x = (x * Text_GetScaleH(PHD_ONE)) / PHD_ONE;
+    result->pos.y = (y * Text_GetScaleV(PHD_ONE)) / PHD_ONE;
+    result->pos.z = z;
+
+    result->letter_spacing = 1;
+    result->word_spacing = 6;
+
+    result->text_flags = 0;
+    result->outl_flags = 0;
+    result->bgnd_flags = 0;
+    result->bgnd_size.x = 0;
+    result->bgnd_size.y = 0;
+    result->bgnd_off.x = 0;
+    result->bgnd_off.y = 0;
+    result->bgnd_off.z = 0;
+    result->flags.active = 1;
+    g_TextstringCount++;
+
+    strncpy(result->text, text, TEXT_MAX_STRING_SIZE);
+    result->text[TEXT_MAX_STRING_SIZE - 1] = '\0';
+
+    return result;
 }

src/game/text.h
@@ -1,3 +1,7 @@
 #pragma once
 
+#include "global/types.h"
+
 void __cdecl Text_Init(void);
+struct TEXTSTRING *__cdecl Text_Create(
+    int32_t x, int32_t y, int32_t z, const char *string);

src/global/funcs.h
@@ -507,24 +507,23 @@
 #define SpiderLeap ((void __cdecl (*)(int16_t item_num, int16_t angle))0x0043FFC0)
 #define SpiderControl ((void __cdecl (*)(int16_t item_num))0x00440070)
 #define BigSpiderControl ((void __cdecl (*)(int16_t item_num))0x00440290)
-#define Text_Create ((TEXTSTRING *__cdecl (*)(int32_t x, int32_t y, int32_t z, const char *string))0x00440480)
-#define Text_ChangeText ((void __cdecl (*)(struct TEXTSTRING *textstring, const char *string))0x00440590)
-#define Text_SetScale ((void __cdecl (*)(struct TEXTSTRING *textstring, int32_t scale_h, int32_t scale_v))0x004405D0)
-#define Text_Flash ((void __cdecl (*)(struct TEXTSTRING *textstring, int16_t enable, int16_t rate))0x004405F0)
-#define Text_AddBackground ((void __cdecl (*)(struct TEXTSTRING *textstring, int16_t x_size, int16_t y_size, int16_t x_off, int16_t y_off, int16_t z_off, int16_t colour, uint16_t *gour_ptr, uint16_t flags))0x00440620)
-#define Text_RemoveBackground ((void __cdecl (*)(struct TEXTSTRING *textstring))0x004406B0)
-#define Text_AddOutline ((void __cdecl (*)(struct TEXTSTRING *textstring, int16_t enable, int16_t colour, uint16_t *gour_ptr, uint16_t flags))0x004406C0)
-#define Text_RemoveOutline ((void __cdecl (*)(struct TEXTSTRING *textstring))0x004406F0)
-#define Text_CentreH ((void __cdecl (*)(struct TEXTSTRING *textstring, int16_t enable))0x00440700)
-#define Text_CentreV ((void __cdecl (*)(struct TEXTSTRING *textstring, int16_t enable))0x00440720)
-#define Text_AlignRight ((void __cdecl (*)(struct TEXTSTRING *textstring, int16_t enable))0x00440740)
-#define Text_AlignBottom ((void __cdecl (*)(struct TEXTSTRING *textstring, int16_t enable))0x00440760)
-#define Text_GetWidth ((int32_t __cdecl (*)(struct TEXTSTRING *textstring))0x00440780)
-#define Text_Remove ((int32_t __cdecl (*)(struct TEXTSTRING *textstring))0x00440890)
-#define Text_GetStringLength ((int16_t __cdecl (*)(const char *string))0x004408C0)
+#define Text_ChangeText ((void __cdecl (*)(struct TEXTSTRING *string, const char *text))0x00440590)
+#define Text_SetScale ((void __cdecl (*)(struct TEXTSTRING *string, int32_t scale_h, int32_t scale_v))0x004405D0)
+#define Text_Flash ((void __cdecl (*)(struct TEXTSTRING *string, int16_t enable, int16_t rate))0x004405F0)
+#define Text_AddBackground ((void __cdecl (*)(struct TEXTSTRING *string, int16_t x_size, int16_t y_size, int16_t x_off, int16_t y_off, int16_t z_off, int16_t colour, uint16_t *gour_ptr, uint16_t flags))0x00440620)
+#define Text_RemoveBackground ((void __cdecl (*)(struct TEXTSTRING *string))0x004406B0)
+#define Text_AddOutline ((void __cdecl (*)(struct TEXTSTRING *string, int16_t enable, int16_t colour, uint16_t *gour_ptr, uint16_t flags))0x004406C0)
+#define Text_RemoveOutline ((void __cdecl (*)(struct TEXTSTRING *string))0x004406F0)
+#define Text_CentreH ((void __cdecl (*)(struct TEXTSTRING *string, int16_t enable))0x00440700)
+#define Text_CentreV ((void __cdecl (*)(struct TEXTSTRING *string, int16_t enable))0x00440720)
+#define Text_AlignRight ((void __cdecl (*)(struct TEXTSTRING *string, int16_t enable))0x00440740)
+#define Text_AlignBottom ((void __cdecl (*)(struct TEXTSTRING *string, int16_t enable))0x00440760)
+#define Text_GetWidth ((int32_t __cdecl (*)(struct TEXTSTRING *string))0x00440780)
+#define Text_Remove ((int32_t __cdecl (*)(struct TEXTSTRING *string))0x00440890)
+#define Text_GetStringLength ((int16_t __cdecl (*)(const char *text))0x004408C0)
 #define Text_Draw ((void __cdecl (*)(void))0x004408F0)
 #define Text_DrawBorder ((void __cdecl (*)(int32_t x, int32_t y, int32_t z, int32_t width, int32_t height))0x00440920)
-#define T_DrawText ((void __cdecl (*)(struct TEXTSTRING *textstring))0x00440AB0)
+#define T_DrawText ((void __cdecl (*)(struct TEXTSTRING *string))0x00440AB0)
 #define Text_GetScaleH ((uint32_t __cdecl (*)(uint32_t scale_h))0x00440E90)
 #define Text_GetScaleV ((uint32_t __cdecl (*)(uint32_t scale_v))0x00440ED0)
 #define MineControl ((void __cdecl (*)(int16_t mine_num))0x00440F10)

src/global/types.h
@@ -269,6 +269,7 @@ typedef struct SPHERE {
 typedef struct TEXTSTRING {
     union {
         uint32_t all;
+        uint32_t active : 1;
     } flags;
     uint16_t text_flags;
     uint16_t bgnd_flags;
@@ -301,7 +302,7 @@ typedef struct TEXTSTRING {
         int32_t h;
         int32_t v;
     } scale;
-    char *string;
+    char *text;
 } TEXTSTRING;
 
 typedef struct PHD_VBUF {

src/global/vars.h
@@ -121,6 +121,7 @@
 #define g_CD_LoopTrack (*(int32_t*)0x0051BDB0)
 #define g_TextstringTable (*(struct TEXTSTRING(*)[64])0x0051C820)
 #define g_TextstringCount (*(int16_t*)0x0051D6A0) // = 0
+#define g_TextstringBuffers (*(char(*)[64][64])0x0051D6C0)
 #define g_SoundIsActive (*(int32_t*)0x0051E6C4)
 #define g_Lara (*(struct LARA_INFO*)0x005206E0)
 #define g_LaraItem (*(struct ITEM_INFO **)0x005207BC)

src/inject_exec.c
@@ -96,6 +96,7 @@ static void Inject_Shell(void)
 static void Inject_Text(void)
 {
     INJECT(1, 0x00440450, Text_Init);
+    INJECT(1, 0x00440480, Text_Create);
 }
 
 static void Inject_Output(void)

tools/generate_funcs
@@ -112,12 +112,14 @@ def make_func_pointer_define(function: Symbol) -> str:
 
 def make_var_pointer_define(variable: Symbol) -> str:
     if match := re.match(
-        r"^(?P<ret_type>.+?)\s*"
+        r"^"
+        r"(?P<ret_type>.+?)\s*"
         r"\("
         r"\s*\*(?P<call_type>.*?\s)\s*(?P<func_name>\w+)"
-        r"(?P<array_def>\[[^\]]*?\])?"
+        r"(?P<array_def>\[[^\]]*?\]+)?"
         r"\)\s*"
-        r"\((?P<args>.+)\);?$",
+        r"\((?P<args>.+)\)"
+        r";?$",
         variable.signature,
     ):
         ret_type = match.group("ret_type")
@@ -131,7 +133,12 @@ def make_var_pointer_define(variable: Symbol) -> str:
             return f"#define {func_name} (*({ret_type}({call_type}**)({args})){variable.offset_str})"
 
     if match := re.match(
-        r"^(?P<ret_type>.+?)\s*(?P<var_name>\w+)\s*(?P<array_def>\[[^\]]*?\])?(\s*=\s*(?P<value_def>.*?))?;?$",
+        r"^"
+        r"(?P<ret_type>.+?)\s*"
+        r"(?P<var_name>\b\w+)\s*"
+        r"(?P<array_def>(?:\[[^\]]*?\])*)"
+        r"(\s*=\s*(?P<value_def>.*?))?"
+        r";?$",
         variable.signature,
     ):
         ret_type = match.group("ret_type")

commit deec090106ae2593ddb36cd524ccbcbd4e7cbfbc

text: fix Text_Create not clearing all flags
Regression from bd005bf, caused requesters to be often drawn in a
glitchy way

src/game/text.c
@@ -40,21 +40,21 @@ struct TEXTSTRING *__cdecl Text_Create(
         return NULL;
     }
 
-    struct TEXTSTRING *result = NULL;
+    int32_t free_idx = -1;
     for (int i = 0; i < TEXT_MAX_STRINGS; i++) {
         struct TEXTSTRING *const string = &g_TextstringTable[i];
         if (!string->flags.active) {
-            result = string;
+            free_idx = i;
             break;
         }
     }
 
-    if (result == NULL) {
+    if (free_idx == -1) {
         return NULL;
     }
 
-    const int32_t i = result - g_TextstringTable;
-    result->text = g_TextstringBuffers[i];
+    struct TEXTSTRING *result = &g_TextstringTable[free_idx];
+    result->text = g_TextstringBuffers[free_idx];
     result->scale.h = PHD_ONE;
     result->scale.v = PHD_ONE;
     result->pos.x = (x * Text_GetScaleH(PHD_ONE)) / PHD_ONE;
@@ -72,6 +72,7 @@ struct TEXTSTRING *__cdecl Text_Create(
     result->bgnd_off.x = 0;
     result->bgnd_off.y = 0;
     result->bgnd_off.z = 0;
+    result->flags.all = 0;
     result->flags.active = 1;
     g_TextstringCount++;
 